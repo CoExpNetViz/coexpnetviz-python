4b7af2956dd80b6f0587828f27677a0b938ebfc1
Tim Diels <timdiels.m@gmail.com>
2016-01-02 15:47:08 +0100

    Add forgotten copyright

--- python3/src/deep_blue_genome/test/performance/test_single_list_containers.py
+++ python3/src/deep_blue_genome/test/performance/test_single_list_containers.py
@@ -1,3 +1,19 @@
+# Copyright (C) 2015 VIB/BEG/UGent - Tim Diels <timdiels.m@gmail.com>
+# 
+# This file is part of Deep Blue Genome.
+# 
+# Deep Blue Genome is free software: you can redistribute it and/or modify
+# it under the terms of the GNU Lesser General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+# 
+# Deep Blue Genome is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU Lesser General Public License for more details.
+# 
+# You should have received a copy of the GNU Lesser General Public License
+# along with Deep Blue Genome.  If not, see <http://www.gnu.org/licenses/>.
 
 # TODO may be set does not belong in this list. Also note that Series does not have set operations, but pd.Index does
 '''
