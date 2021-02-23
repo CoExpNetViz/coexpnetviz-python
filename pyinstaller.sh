39ce8dec7793f9aa2579285e684451adef7eaecb
Tim Diels <tim@diels.me>
2021-02-23 15:44:58 +0100

    Revert "Add pyinstaller build command and hooks (#3)"
    This reverts commit f47648349c4697bcabe1208c7135a369439e9553.

--- pyinstaller.sh
+++
@@ -1,2 +0,0 @@
-#!/usr/bin/env bash
-pyinstaller --onefile --additional-hooks-dir=pyinstaller coexpnetviz/main.py
--- pyinstaller/hook-humanize.py
+++
@@ -1,3 +0,0 @@
-# Solve pkg_resources.DistributionNotFound https://stackoverflow.com/a/60529533
-from PyInstaller.utils.hooks import copy_metadata
-datas = copy_metadata('humanize')
