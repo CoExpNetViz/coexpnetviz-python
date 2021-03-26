6da21ac2976d7d6ea44f697058952702b9a1259e
Tim Diels <tim@diels.me>
2021-03-26 23:13:00 +0100

    Bump to v5.0.1.dev1

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 name = 'coexpnetviz'
 setup(
-    version='5.0.0',
+    version='5.0.1.dev1',
     name=name,
     entry_points={'console_scripts': [
         'coexpnetviz = coexpnetviz.main:main'
