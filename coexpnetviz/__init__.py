ca61acca9b8e7c14ac443a5fb2136740c56f1855
Tim Diels <tim@diels.me>
2021-03-26 17:58:30 +0100

    Bump to v5.0.0

--- coexpnetviz/__init__.py
+++ coexpnetviz/__init__.py
@@ -15,4 +15,4 @@
 # You should have received a copy of the GNU Lesser General Public License
 # along with CoExpNetViz.  If not, see <http://www.gnu.org/licenses/>.
 
-__version__ = '5.0.0.dev1'
+__version__ = '5.0.0'
