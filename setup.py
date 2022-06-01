99be8e9ea39d2a6180f33d3ab5aeb196952441b3
Tim Diels <tim@diels.me>
2022-06-01 18:19:20 +0200

    Forgot to bump version in setup.py

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 name = 'coexpnetviz'
 setup(
-    version='6.0.3',
+    version='6.0.4',
     name=name,
     entry_points={'console_scripts': [
         'coexpnetviz = coexpnetviz.main:main'
